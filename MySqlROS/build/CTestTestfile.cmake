ac58b62b240bb11d4d4abd6b98e7d75f05d18e9f
Viktor <vikpo@live.dk>
2018-09-27 11:01:12 +0200

    Added ROSJava Package and JDBC software

---
+++ MySqlROS/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/viktor/Desktop/3_sempro/MySqlROS/src
+# Build directory: /home/viktor/Desktop/3_sempro/MySqlROS/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(rosjava_catkin_package_a)
---
+++ MySqlROS/build/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/gmock
+# Build directory: /home/viktor/Desktop/3_sempro/MySqlROS/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
