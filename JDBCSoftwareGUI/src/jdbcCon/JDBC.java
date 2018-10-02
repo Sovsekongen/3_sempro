e7bf235b60573f8a35fc156f64c5a11abd001410
Viktor Poulsen <vikpo@live.dk>
2018-10-02 12:19:22 +0200

    Changed java project to java fxml-type to easier build GUI

---
+++ JDBCSoftwareGUI/src/jdbcCon/JDBC.java
@@ -0,0 +1,110 @@
+package jdbcCon;
+
+import java.sql.*;
+
+public class JDBC
+{
+
+    private Statement stmt = null;
+    private ResultSet rs = null;
+    private Connection con = null;
+
+    private String url = "jdbc:mysql://localhost:3306/bib?useSSL=false&serverTimezone=EST";
+    private String usr;
+    private String pwd;
+
+    public JDBC(String usr, String pwd)
+    {
+        this.usr = usr;
+        this.pwd = pwd;
+        
+        try
+        {
+            con = DriverManager.getConnection(url, usr, pwd);
+
+        } 
+        catch (SQLException e)
+        {
+            System.out.println("SQLException: " + e.getMessage());
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("VendorError: " + e.getErrorCode());
+        }
+    }
+
+    public void selectQuery(String query)
+    {
+        try
+        {
+            stmt = con.createStatement();
+            rs = stmt.executeQuery(query);
+            
+            ResultSetMetaData rsmd = rs.getMetaData();
+
+            int columns = rsmd.getColumnCount();
+            
+            while (rs.next())
+            {
+                for(int i = 1; i < columns; i++)
+                {
+                    if(i > 1)
+                    {
+                        System.out.print("; ");
+                    }
+                    
+                    String columnName = rs.getString(i);
+                    System.out.print(rsmd.getColumnName(i) + ": " + columnName);
+                    
+                }
+                System.out.println(" ");
+            }
+        } 
+        catch (SQLException e)
+        {
+            System.out.println("SQLException: " + e.getMessage());
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("VendorError: " + e.getErrorCode());
+        }
+    }
+
+    public void insertPickupPos(String date, int posx, int posy)
+    {
+        try
+        {
+            stmt = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
+            ResultSet.CONCUR_UPDATABLE);
+            rs = stmt.executeQuery("SELECT * FROM book;");     
+            
+            rs.moveToInsertRow();
+            rs.updateString("", date);
+            rs.updateInt("", posx);
+            rs.updateInt("", posy);
+            
+            rs.insertRow();
+            rs.beforeFirst();
+        }
+        catch(SQLException e)
+        {
+            System.out.println("SQLException: " + e.getMessage());
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("VendorError: " + e.getErrorCode());
+        }
+        finally
+        {
+            if(stmt != null) 
+            {
+                try
+                {
+                    stmt.close();
+                }
+                catch(SQLException e)
+                {
+                    System.out.println("SQLException: " + e.getMessage());
+                    System.out.println("SQLState: " + e.getSQLState());
+                    System.out.println("VendorError: " + e.getErrorCode());
+                } 
+            }
+        }
+    }
+    
+    
+}
