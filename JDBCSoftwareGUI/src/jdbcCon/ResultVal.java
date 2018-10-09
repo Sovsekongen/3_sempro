6f2b29f57318d2f5e76d948fd7e3b9881251b1b9
Viktor Poulsen <vikpo@live.dk>
2018-10-09 08:47:28 +0200

    Redid sum shit

--- JDBCSoftwareGUI/src/jdbcCon/ResultVal.java
+++ JDBCSoftwareGUI/src/jdbcCon/ResultVal.java
@@ -2,5 +2,32 @@ package jdbcCon;
 
 public class ResultVal 
 {
+    private int points;
+    private double distanceFromO;
 
+    public ResultVal(int points, double distanceFromO)
+    {
+        this.points = points;
+        this.distanceFromO = distanceFromO;
+    }
+
+    public int getPoints()
+    {
+        return points;
+    }
+
+    public void setPoints(int points)
+    {
+        this.points = points;
+    }
+
+    public double getDistanceFromO()
+    {
+        return distanceFromO;
+    }
+
+    public void setDistanceFromO(double distanceFromO)
+    {
+        this.distanceFromO = distanceFromO;
+    }
 }
--- JDBCSoftwareGUI/src/jdbcCon/TimeVal.java
+++ JDBCSoftwareGUI/src/jdbcCon/TimeVal.java
@@ -1,6 +1,62 @@
 package jdbcCon;
 
+import java.util.logging.Logger;
+
 public class TimeVal 
 {
 
+    private int pickUpTime;
+    private int imagePTime;
+    private int throwTime;
+    private int totalTime;
+
+    public TimeVal(int pickUpTime, int imagePTime, int throwTime, int totalTime)
+    {
+        this.pickUpTime = pickUpTime;
+        this.imagePTime = imagePTime;
+        this.throwTime = throwTime;
+        this.totalTime = totalTime;
+    }
+
+    public int getPickUpTime()
+    {
+        return pickUpTime;
+    }
+
+    public void setPickUpTime(int pickUpTime)
+    {
+        this.pickUpTime = pickUpTime;
+    }
+
+    public int getImagePTime()
+    {
+        return imagePTime;
+    }
+
+    public void setImagePTime(int imagePTime)
+    {
+        this.imagePTime = imagePTime;
+    }
+
+    public int getThrowTime()
+    {
+        return throwTime;
+    }
+
+    public void setThrowTime(int throwTime)
+    {
+        this.throwTime = throwTime;
+    }
+
+    public int getTotalTime()
+    {
+        return totalTime;
+    }
+
+    public void setTotalTime(int totalTime)
+    {
+        this.totalTime = totalTime;
+    }
+    
+    
 }
